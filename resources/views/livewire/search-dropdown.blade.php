918b75a58dc56da8837ee60feb956fae9dd3ffdc
Vladislavs Pozņaks <vladislavs.poznaks@gmail.com>
2020-10-20 16:16:17 +0300

    search dropdown UX added

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,9 +1,20 @@
-<div class="relative">
+<div class="relative" x-data="{ isVisible: true }" @click.away="isVisible = false">
     <input
         wire:model.debounce.300ms="search"
         type="text"
         class="bg-gray-800 text-sm rounded-full focus:outline-none focus:shadow-outline w-64 px-3 pl-8 py-1"
-        placeholder="Search..."
+        placeholder="Search (Press '/' to focus)"
+        x-ref="search"
+        @keydown.window="
+            if (event.keyCode === 191) {
+                event.preventDefault();
+                $refs.search.focus();
+            }
+        "
+        @focus="isVisible = true"
+        @keydown.escape.window = "isVisible = false"
+        @keydown="isVisible = true"
+        @keydown.shift.tab="isVisible = false"
     >
     <div class="absolute top-0 flex items-center h-full ml-2">
         <svg class="fill-current text-gray-400 w-4" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z" /></svg>
@@ -15,12 +26,21 @@
 
 
     @if(strlen($search) > 2)
-        <div class="absolute z-50 bg-gray-800 text-xs rounded w-64 mt-2">
+        <div
+            class="absolute z-50 bg-gray-800 text-xs rounded w-64 mt-2"
+            x-show.transition.opacity.duration.200="isVisible"
+        >
             @if (count($results) !== 0)
                 <ul>
                     @foreach($results as $result)
                     <li class="border-b border-gray-700">
-                        <a href="{{ route('games.show', $result['slug']) }}" class="block hover:bg-gray-700 flex items-center transition ease-in-out duration-150 px-3 py-3">
+                        <a
+                            href="{{ route('games.show', $result['slug']) }}"
+                            class="block hover:bg-gray-700 flex items-center transition ease-in-out duration-150 px-3 py-3"
+                            @if($loop->last)
+                                @keydown.tab="isVisible = false"
+                            @endif
+                        >
                             <img
                                 src="{{ $result['cover_small_url'] }}"
                                 alt="{{ $result['name'] }}'s cover"
